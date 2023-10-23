2587c24e31064f850f8f9a91df7caf7148983ce7
Grayson Miller <grayson.miller124@gmail.com>
2023-10-23 15:43:01 -0600

    refactored demos for dw

---
+++ demo/dw/__main__.py
@@ -0,0 +1,35 @@
+import dataclass_wizard
+import json 
+
+from lilvali import validate, validator, ValidationError
+
+@dataclasses
+class JsonSchema:
+    """ a dataclass that defines a json schema. """
+
+
+
+class JsonBuilder:
+
+
+
+@validate
+def add[T: (int, float)](x: int, y: T) -> int | float:
+    return x + y
+
+
+def main():
+    print(f"{add(1, 2)=}")
+    print(f"{add(1, 2.0)=}")
+
+    # TODO: Bug: should raise ValidationError
+    try:
+        print(f"{add(1.0, 2)=}")
+    except ValidationError as e:
+        print(f"{e=}")
+    else:
+        raise RuntimeError("Expected ValidationError")
+
+
+if __name__ == "__main__":
+    main()
